412aac416dbd2bb9892ba9a87fa4594f22c58e52
Yura Zhloba <yzh44yzh@gmail.com>
2015-02-07 22:54:19 +0300

    work with lesson 03, foldl

--- lesson_03/main.erl
+++ lesson_03/main.erl
@@ -1,7 +1,8 @@
 -module(main).
 
--export([get_users/0, get_females/1, split_by_age/1, get_id_name/1,
-         get_females_id_name/1, get_females_id_name2/1]).
+-export([get_users/0, get_females/1, get_id_name/1,
+         get_females_id_name/1, get_females_id_name2/1, get_stat/1,
+         split_by_age/1]).
 
 get_users() ->
     [{user, 1, "Bob", male, 22},
@@ -15,11 +16,6 @@ get_females(Users) ->
            ({user, _, _, female, _}) -> true
         end,
     lists:filter(F, Users).
-
-
-split_by_age(Users) ->
-    F = fun({user, _, _, _, Age}) -> Age < 18 end,
-    lists:partition(F, Users).
 
 
 get_id_name(Users) ->
@@ -36,3 +32,25 @@ get_females_id_name2(Users) ->
     lists:filtermap(fun({user, _, _, male, _}) -> false;
                        ({user, Id, Name, female, _}) -> {true, {Id, Name}}
                     end, Users).
+
+
+get_stat(Users) ->
+    F = fun({user, _, _, Gender, Age}, {Males, Females, TotalUsers, TotalAge}) ->
+                case Gender of
+                    male -> {Males + 1, Females, TotalUsers + 1, TotalAge + Age};
+                    female -> {Males, Females + 1, TotalUsers + 1, TotalAge + Age}
+                end
+        end,
+    lists:foldl(F, {0, 0, 0, 0}, Users).
+
+
+split_by_age(Users) ->
+    lists:foldl(fun(User, {Acc1, Acc2}) ->
+                        {user, _, _, _, Age} = User,
+                        if
+                            Age < 18 -> {[User | Acc1], Acc2};
+                            true -> {Acc1, [User | Acc2]}
+                        end
+                end,
+                {[], []},
+                Users).
