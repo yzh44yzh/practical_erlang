dac8c0569222a6ef5b33e4c1eda311853f0b283a
Yura Zhloba <yzh44yzh@gmail.com>
2016-03-27 21:54:19 +0300

    work on ex 16

--- 16_sockets/solution/src/mcache_server.erl
+++ 16_sockets/solution/src/mcache_server.erl
@@ -19,10 +19,91 @@ accept(Id, ListenSocket) ->
 
 handle_connection(Id, ListenSocket, Socket) ->
     case gen_tcp:recv(Socket, 0) of
-        {ok, Msg} -> io:format("Socket #~p got message: ~p~n", [Id, Msg]),
-                     gen_tcp:send(Socket, Msg),
-                     handle_connection(Id, ListenSocket, Socket);
+        {ok, Msg0} -> io:format("Socket #~p got message: ~p~n", [Id, Msg0]),
+                      Msg = binary:part(Msg0, 0, byte_size(Msg0) - 2),
+                      Reply = handle(parse_protocol(Msg)),
+                      gen_tcp:send(Socket, <<Reply/binary, "\r\n">>),
+                      handle_connection(Id, ListenSocket, Socket);
         {error, closed} ->
             io:format("Socket #~p, session closed ~n", [Id]),
             accept(Id, ListenSocket)
     end.
+
+
+parse_protocol(Data) ->
+    Parts = binary:split(Data, <<" ">>),
+    case Parts of
+        [<<"SET">>, Rest] -> key_value(set, Rest);
+        [<<"GET">>, Key] -> {get, Key};
+        [<<"GETS">>, Keys] -> {gets, binary:split(Keys, <<" ">>, [global])};
+        [<<"ADD">>, Rest] -> key_value(add, Rest);
+        [<<"REPLACE">>, Rest] -> key_value(replace, Rest);
+        [<<"APPEND">>, Rest] -> key_value(append, Rest);
+        [<<"PREPEND">>, Rest] -> key_value(prepend, Rest);
+        [<<"DELETE">>, Key] -> {delete, Key};
+        _ -> unknown
+    end.
+
+
+key_value(Action, Data) ->
+    case binary:split(Data, <<" ">>) of
+        [Key, Value] -> {Action, Key, Value};
+        _ -> unknown
+    end.
+
+
+handle({set, Key, Value}) ->
+    mcache:set(Key, Value),
+    <<"STORED">>;
+
+handle({get, Key}) ->
+    case mcache:get(Key) of
+        {ok, Value} -> <<"VALUE ", Key/binary, " ", Value/binary, "\r\nEND">>;
+        {error, not_found} -> <<"NOT FOUND">>
+    end;
+
+handle({gets, Keys}) ->
+    {[], Res} = lists:foldl(
+                  fun(R, {[Key | Rest], Acc}) ->
+                          Value = case R of
+                                      {ok, V} -> V;
+                                      {error, not_found} -> <<"NOT FOUND">>
+                                  end,
+                          {Rest, <<Acc/binary, "VALUE ", Key/binary, " ", Value/binary, "\r\n">>}
+                  end,
+                  {Keys, <<>>},
+                  mcache:gets(Keys)),
+    <<Res/binary, "END">>;
+
+handle({add, Key, Value}) ->
+    case mcache:add(Key, Value) of
+        ok -> <<"STORED">>;
+        {error, exists} -> <<"EXISTS">>
+    end;
+
+handle({replace, Key, Value}) ->
+    case mcache:replace(Key, Value) of
+        ok -> <<"STORED">>;
+        {error, not_found} -> <<"NOT FOUND">>
+    end;
+
+handle({append, Key, Value}) ->
+    case mcache:append(Key, Value) of
+        ok -> <<"STORED">>;
+        {error, not_found} -> <<"NOT FOUND">>
+    end;
+
+handle({prepend, Key, Value}) ->
+    case mcache:prepend(Key, Value) of
+        ok -> <<"STORED">>;
+        {error, not_found} -> <<"NOT FOUND">>
+    end;
+
+handle({delete, Key}) ->
+    case mcache:delete(Key) of
+        ok -> <<"DELETED">>;
+        {error, not_found} -> <<"NOT FOUND">>
+    end;
+
+handle(unknown) ->
+    <<"UKNOWN REQUEST">>.
