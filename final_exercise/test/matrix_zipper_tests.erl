17a92b5a148d1cea4ef3c58e9c351ca78e883f92
Yura Zhloba <yzh44yzh@gmail.com>
2016-05-21 17:44:45 +0300

    matrix zipper tests

--- final_exercise/test/matrix_zipper_tests.erl
+++ final_exercise/test/matrix_zipper_tests.erl
@@ -85,11 +85,43 @@ position_test() ->
                {down, 10}, {get, 16}, {pos, {4, 4}},
                {left, 1}, {get, 15}, {pos, {3, 4}},
                {up, 2}, {get, 7}, {pos, {3, 2}},
-               {left, 10}, {get, 5}, {pos, {1, 2}}, % invalid move
-               {up, 10}, {get, 5}, {pos, {1, 2}}, % invalid move
-               {up, 1}, {get, 1}, {pos, {1, 1}}
+               {left, 10}, {get, 7}, {pos, {3, 2}}, % invalid move
+               {up, 10}, {get, 7}, {pos, {3, 2}}, % invalid move
+               {up, 1}, {get, 3}, {pos, {3, 1}}
               ],
     lists:foldl(fun check/2, Z, Actions),
+    ok.
+
+
+find_test() ->
+    Z = matrix_zipper:from_matrix(
+           [[ 1, 2, 3, 4],
+            [ 5, 6, 7, 8],
+            [ 9,10,11,12],
+            [13,14,15,16]]
+           ),
+    {error, not_found} = matrix_zipper:find(Z, 20),
+    {error, not_found} = matrix_zipper:find(Z, 30),
+    {error, not_found} = matrix_zipper:find(Z, -1),
+
+    lists:foreach(
+      fun(Num) ->
+              {ok, ZZ} = matrix_zipper:find(Z, Num),
+              ?assertEqual(Num, matrix_zipper:get(ZZ))
+      end,
+      lists:seq(1, 16)),
+
+    {ok, Z2} = matrix_zipper:find(Z, 11),
+    {ok, Z3} = matrix_zipper:find_left(Z2, 10), ?assertEqual(10, matrix_zipper:get(Z3)),
+    {ok, Z4} = matrix_zipper:find_left(Z2, 9), ?assertEqual(9, matrix_zipper:get(Z4)),
+    {error, not_found} = matrix_zipper:find_left(Z2, 8),
+    {ok, Z5} = matrix_zipper:find_right(Z2, 12), ?assertEqual(12, matrix_zipper:get(Z5)),
+    {error, not_found} = matrix_zipper:find_right(Z2, 13),
+    {ok, Z6} = matrix_zipper:find_up(Z2, 7), ?assertEqual(7, matrix_zipper:get(Z6)),
+    {ok, Z7} = matrix_zipper:find_up(Z2, 3), ?assertEqual(3, matrix_zipper:get(Z7)),
+    {error, not_found} = matrix_zipper:find_up(Z2, 2),
+    {ok, Z8} = matrix_zipper:find_down(Z2, 15), ?assertEqual(15, matrix_zipper:get(Z8)),
+    {error, not_found} = matrix_zipper:find_down(Z2, 16),
     ok.
 
 
