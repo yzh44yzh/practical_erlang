cedede7e19990fb070522c5385ce0a64eff95e2b
Yuriy Zhloba <y_zhloba@wargaming.net>
2018-09-04 23:10:32 +0300

    better solution for 11_link_monitor

--- 11_link_monitor/solution/main.erl
+++ 11_link_monitor/solution/main.erl
@@ -1,42 +1,74 @@
 -module(main).
 
--export([parse/1]).
+-export([parse/1, parser/2]).
 
+%% Root process
 
 parse(Files) ->
-    Workers = lists:map(
-                fun(File) ->
-                        Pid = spawn(worker, parse, [File, self()]),
-                        Ref = erlang:monitor(process, Pid),
-                        {Ref, File}
-                end, Files),
-    wait_results(Workers, {#{}, #{}}).
+    Workers = lists:foldl(
+        fun(File, Acc) ->
+            Pid = spawn(?MODULE, parser, [File, self()]),
+            Ref = erlang:monitor(process, Pid),
+            Acc#{{Pid, Ref} => File}
+        end,
+        #{},
+        Files),
+    wait_for_data(Workers, {#{}, #{}}).
 
 
-wait_results([], {Data, Errors}) -> {Data, Errors};
-wait_results(Workers, {Data, Errors}) ->
+wait_for_data(Workers, Acc) when map_size(Workers) == 0 -> Acc;
+wait_for_data(Workers, {DataAcc, ErrAcc}) ->
     receive
-        {ok, Result} ->
-            Data2 = add_result(Result, Data),
-            wait_results(Workers, {Data2, Errors});
-        {'DOWN', Ref, process, _Pid, normal} ->
-            Workers2 = lists:keydelete(Ref, 1, Workers),
-            wait_results(Workers2, {Data, Errors});
-        {'DOWN', Ref, process, _Pid, Error} ->
-            {Ref, File} = lists:keyfind(Ref, 1, Workers),
-            Workers2 = lists:delete({Ref, File}, Workers),
-            Errors2 = maps:put(File, Error, Errors),
-            wait_results(Workers2, {Data, Errors2})
+        {result, Data} ->
+            DataAcc2 = aggregate(Data, DataAcc),
+            wait_for_data(Workers, {DataAcc2, ErrAcc});
+        {'DOWN', Ref, process, Pid, Reason} ->
+            Workers2 = maps:remove({Pid, Ref}, Workers),
+            case Reason of
+                normal -> wait_for_data(Workers2, {DataAcc, ErrAcc});
+                _ ->
+                    File = maps:get({Pid, Ref}, Workers),
+                    ErrAcc2 = ErrAcc#{File => Reason},
+                    wait_for_data(Workers2, {DataAcc, ErrAcc2})
+            end
     after
-        2000 -> no_reply
+        1000 -> {error, no_reply}
     end.
 
 
-add_result(Items, Data) ->
+%% Parser process
+
+parser(File, RootPid) ->
+    {ok, Bin} = file:read_file(File),
+    Lines = binary:split(Bin, <<"\n">>, [global]),
+    Data = parse_lines(Lines),
+    RootPid ! {result, Data},
+    ok.
+
+
+%% Utils
+
+parse_lines(Lines) ->
     lists:foldl(
-      fun({Item, Quantity}, Acc) ->
-              case maps:find(Item, Acc) of
-                  {ok, Val} -> maps:put(Item, Quantity + Val, Acc);
-                  error -> maps:put(Item, Quantity, Acc)
-              end
-      end, Data, Items).
+        fun
+            (<<>>, Acc) -> Acc;
+            (Line, Acc) ->
+                [_Id, Name, Quantity, _Price]
+                    = binary:split(Line, <<",">>, [global]),
+                Quantity2 = list_to_integer(binary_to_list(Quantity)),
+                Acc#{Name => Quantity2}
+        end,
+        #{},
+        Lines).
+
+
+aggregate(Data1, Data2) ->
+    maps:fold(
+        fun(Name, Quantity, Acc) ->
+            case maps:find(Name, Data2) of
+                {ok, Quantity2} -> Acc#{Name => Quantity + Quantity2};
+                error -> Acc#{Name => Quantity}
+            end
+        end,
+        Data2,
+        Data1).
